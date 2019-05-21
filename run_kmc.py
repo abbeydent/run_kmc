5e05a213afbdbf99baf8d02029476a0356c3c906
Abbey Dent <adent@g.clemson.edu>
2019-05-21 13:25:19 -0400

    Creating scatterplot from generated dataset

--- run_kmc.py
+++ run_kmc.py
@@ -6,7 +6,8 @@ from sklearn.cluster import KMeans
 
 X,Y = make_blobs(n_samples=400, centers=4, cluster_std=0.60, random_state=0)
 
-print(X)
+#print(X)
 
-
+plt.scatter(X[:,0], X[:,1])
+plt.savefig('scatterplot.png')
 
