c0a250ea5da1255392d7af57943e0fe4ffdc9e7a
Abbey Dent <adent@g.clemson.edu>
2019-05-21 13:24:00 -0400

    Generating dataset

---
+++ run_kmc.py
@@ -0,0 +1,12 @@
+from sklearn.datasets.samples_generator import make_blobs
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+from sklearn.cluster import KMeans
+
+X,Y = make_blobs(n_samples=400, centers=4, cluster_std=0.60, random_state=0)
+
+print(X)
+
+
+
