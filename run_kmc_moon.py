d461a34a05f36b186211a9c39edf4ec0533ee894
Abbey Dent <adent@g.clemson.edu>
2019-05-21 13:37:35 -0400

    Creating scatterplot with moon data

--- run_kmc_moon.py
+++ run_kmc_moon.py
@@ -6,4 +6,8 @@ from sklearn.cluster import KMeans
 
 X,Y = make_moons(n_samples=400, noise=0.05, random_state=0)
 
-print(X)
+#print(X)
+
+plt.scatter(X[:,0], X[:,1])
+plt.savefig('scatterplot_moon.png')
+
